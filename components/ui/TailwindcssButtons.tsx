34b04be3a52d40197b04c3949c2a141a3577ef18
unknown <pikinleon39@gmail.com>
2025-05-28 00:28:53 +0100

    make changes on the page overflow and magicbutton

--- components/ui/TailwindcssButtons.tsx
+++ components/ui/TailwindcssButtons.tsx
@@ -15,7 +15,7 @@ const TailwindcssButtons = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+      className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none"
       onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
